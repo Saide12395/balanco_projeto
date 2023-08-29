f4113d7ae2e900a0ba86bf75040dfbad12c8113c
Saide12395 <Cjumila@isced.ac.mz>
2023-08-29 16:03:29 +0300

    add static css commit

--- balanco/urls.py
+++ balanco/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.balanco_patrimonial, name='balanco_patrimonial'),
     path('ativos/', views.ativos_listagem, name='ativos_listagem'),
-    path('passivos/', views.passivos_listagem, name='passivos_listagem'),
     path('adicionar-ativo/', views.adicionar_ativo, name='adicionar_ativo'),
-    path('adicionar-passivo/', views.adicionar_passivo, name='adicionar_passivo'),
     path('editar-ativo/<int:ativo_id>/', views.editar_ativo, name='editar_ativo'),
     path('apagar-ativo/<int:ativo_id>/', views.apagar_ativo, name='apagar_ativo'),
-
-
+    path('passivos/', views.passivos_listagem, name='passivos_listagem'),
+    path('adicionar-passivo/', views.adicionar_passivo, name='adicionar_passivo'),
+    path('editar-passivo/<int:passivo_id>/', views.editar_passivo, name='editar_passivo'),
+    path('apagar-passivo/<int:passivo_id>/', views.apagar_passivo, name='apagar_passivo'),
 ]
--- balanco/views.py
+++ balanco/views.py
@@ -2,28 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Ativo, Passivo
 from .forms import AtivoForm, PassivoForm
 
-def balanco_patrimonial(request):
-    ativos = Ativo.objects.all()
-    passivos = Passivo.objects.all()
-    
-    total_ativos = sum(ativo.valor for ativo in ativos)
-    total_passivos = sum(passivo.valor for passivo in passivos)
-    patrimonio_liquido = total_ativos - total_passivos
-    
-    return render(request, 'balanco/balanco.html', {
-        'total_ativos': total_ativos,
-        'total_passivos': total_passivos,
-        'patrimonio_liquido': patrimonio_liquido,
-    })
-
-def ativos_listagem(request):
-    ativos = Ativo.objects.all()
-    return render(request, 'balanco/ativos_listagem.html', {'ativos': ativos})
-
-def passivos_listagem(request):
-    passivos = Passivo.objects.all()
-    return render(request, 'balanco/passivos_listagem.html', {'passivos': passivos})
-
 def adicionar_ativo(request):
     if request.method == 'POST':
         ativo_form = AtivoForm(request.POST)
@@ -32,18 +10,9 @@ def adicionar_ativo(request):
             return redirect('ativos_listagem')
     else:
         ativo_form = AtivoForm()
+    
     return render(request, 'balanco/adicionar_ativo.html', {'ativo_form': ativo_form})
 
-def adicionar_passivo(request):
-    if request.method == 'POST':
-        passivo_form = PassivoForm(request.POST)
-        if passivo_form.is_valid():
-            passivo_form.save()
-            return redirect('passivos_listagem')
-    else:
-        passivo_form = PassivoForm()
-    return render(request, 'balanco/adicionar_passivo.html', {'passivo_form': passivo_form})
-#Editar
 def editar_ativo(request, ativo_id):
     ativo = get_object_or_404(Ativo, pk=ativo_id)
 
@@ -56,7 +25,7 @@ def editar_ativo(request, ativo_id):
         ativo_form = AtivoForm(instance=ativo)
     
     return render(request, 'balanco/editar_ativo.html', {'ativo_form': ativo_form})
-#Apagar
+
 def apagar_ativo(request, ativo_id):
     ativo = get_object_or_404(Ativo, pk=ativo_id)
     if request.method == 'POST':
@@ -64,4 +33,61 @@ def apagar_ativo(request, ativo_id):
         return redirect('ativos_listagem')
     return render(request, 'balanco/apagar_ativo.html', {'ativo': ativo})
 
+def ativos_listagem(request):
+    ativos = Ativo.objects.all()
+    return render(request, 'balanco/ativos_listagem.html', {'ativos': ativos})
+
+def adicionar_passivo(request):
+    if request.method == 'POST':
+        passivo_form = PassivoForm(request.POST)
+        if passivo_form.is_valid():
+            passivo_form.save()
+            return redirect('passivos_listagem')
+    else:
+        passivo_form = PassivoForm()
+    
+    return render(request, 'balanco/adicionar_passivo.html', {'passivo_form': passivo_form})
+
+def editar_passivo(request, passivo_id):
+    passivo = get_object_or_404(Passivo, pk=passivo_id)
+
+    if request.method == 'POST':
+        passivo_form = PassivoForm(request.POST, instance=passivo)
+        if passivo_form.is_valid():
+            passivo_form.save()
+            return redirect('passivos_listagem')
+    else:
+        passivo_form = PassivoForm(instance=passivo)
+    
+    return render(request, 'balanco/editar_passivo.html', {'passivo_form': passivo_form})
+
+def apagar_passivo(request, passivo_id):
+    passivo = get_object_or_404(Passivo, pk=passivo_id)
+    if request.method == 'POST':
+        passivo.delete()
+        return redirect('passivos_listagem')
+    return render(request, 'balanco/apagar_passivo.html', {'passivo': passivo})
+
+def passivos_listagem(request):
+    passivos = Passivo.objects.all()
+    return render(request, 'balanco/passivos_listagem.html', {'passivos': passivos})
+
+def balanco_patrimonial(request):
+    ativos = Ativo.objects.all()
+    passivos = Passivo.objects.all()
+
+    total_ativos = sum(ativo.valor for ativo in ativos)
+    total_passivos = sum(passivo.valor for passivo in passivos)
+
+    patrimonio_liquido = total_ativos - total_passivos
+
+    context = {
+        'ativos': ativos,
+        'passivos': passivos,
+        'total_ativos': total_ativos,
+        'total_passivos': total_passivos,
+        'patrimonio_liquido': patrimonio_liquido
+    }
+
+    return render(request, 'balanco/balanco_patrimonial.html', context)
 
